cf4f239043fdaa9efe638194f8f98d4102e1a10c
Fabio Bartoli <fabio.bartoli@outlook.com>
2022-06-01 12:19:25 -0300

    Add new branch

--- aula-2/terrafile.tf
+++
@@ -1,8 +0,0 @@
-module "servers" {
-  source = "./servers"
-  servers = 1
-}
-
-output "ip_address" {
-  value = module.servers.ip_address
-}
--- aula-5/servers/output.tf
+++
@@ -1,7 +0,0 @@
-output "ip_address" {
-  #value = aws_instance.web[*].public_ip
-  value = {
-    for instance in aws_instance.web[*] :
-    instance.id => instance.private_ip
-  }
-}
